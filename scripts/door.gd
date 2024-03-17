80a5279b38534001c451dece22d9886b1e570f6e
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-03-17 14:02:14 +0700

    fix door

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,17 +1,18 @@
 extends StaticBody2D
 
-@onready var interaction_area : InteractionArea = $InteractionArea
 @onready var door_anim = $AnimationPlayer
+@onready var interaction_area : InteractionArea = $InteractionArea
+
 
 func _ready():
 	interaction_area.interact = Callable(self, "_change_scene")
 	door_anim.play("close")
-	
+
 func _change_scene() :
 	door_anim.play("open")
 	await door_anim.animation_finished
-	#get_tree().change_scene_to_packed()
-	door_anim.play("close")
+	print("Changed")
+	door_anim.play("close")	
 
 func _on_interaction_area_leave_area():
 	door_anim.play("close")
