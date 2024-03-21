91cc682753a39fb46841eb0d88dd3cc2b693bbf7
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-03-21 15:29:07 +0700

    fix menu

--- scripts/Goblin.gd
+++ scripts/Goblin.gd
@@ -13,6 +13,6 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 	
 func _on_interact():
-	DialogManager.start_dialog(global_position, lines, speech_sound)
+	DialogManager.start_dialog(global_position, lines, speech_sound)	
 	sprite.flip_h = true if interaction_area.get_overlapping_bodies()[0].global_position.x < global_position.x else false
 	await DialogManager.dialog_finished
