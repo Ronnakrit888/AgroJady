f170e8768dd861d56b6fad360a9fb47aedc8e9fd
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-03-22 00:33:54 +0700

    tree state changed 99% work

--- objects_items/scripts/HarvestingTool.gd
+++ objects_items/scripts/HarvestingTool.gd
@@ -13,5 +13,5 @@ func interact_with_body(body):
 	if body is ResourceNode :
 		for type in effected_types:
 			if(body.node_types.has(type)):
-				print_debug("Match found at type " + type.display_name + " on " + body.name)
+				#print_debug("Match found at type " + type.display_name + " on " + body.name)
 				body.harvest(randi_range(min_amount, max_amount))
--- scripts/WorldObjects.gd
+++ scripts/WorldObjects.gd
@@ -1,83 +1,46 @@
 extends Node2D
 
-@onready var marker_summer = $Summer
-@onready var marker_spring = $Spring
-@onready var marker_winter = $Winter
-@onready var marker_autumn = $Autumn
-@onready var tree_cold = preload("res://scenes/resourec_node/resource_node_tree_cold.tscn") 
-@onready var tree_orange = preload("res://scenes/resourec_node/resource_node_tree_orange.tscn")
-@onready var tree_pink = preload("res://scenes/resourec_node/resource_node_tree_pink.tscn")
-@onready var tree_pink2 = preload("res://scenes/resourec_node/resource_node_tree_pink_2.tscn")
-@onready var tree_summer = preload("res://scenes/resourec_node/resource_node_tree_summer.tscn")
+var tree_scenes = {
+	"Summer": preload("res://scenes/resourec_node/resource_node_tree_summer.tscn"),
+	"Spring": preload("res://scenes/resourec_node/resource_node_tree_orange.tscn"),
+	"Winter": preload("res://scenes/resourec_node/resource_node_tree_pink.tscn"),
+	"Autumn": preload("res://scenes/resourec_node/resource_node_tree_cold.tscn")
+	}
 
-var spawned_count_summer : int = 0
-var spawned_count_spring : int = 0
-var spawned_count_winter : int = 0
-var spawned_count_autumn : int = 0
+var spawned_counts = {
+	"Summer" : 0,
+	"Spring" : 0,
+	"Autumn" : 0,
+	"Winter" : 0
+	}
+	
 
 func _ready():
-	spawn_resource(marker_summer ,tree_summer)
-	spawn_resource(marker_spring, tree_orange)
-	spawn_resource(marker_winter, tree_pink)
-	spawn_resource(marker_autumn, tree_cold)
+	for marker_name in tree_scenes.keys():
+		spawn_resource(get_node(marker_name), tree_scenes[marker_name])
 	
-func spawn_resource(node_marker : Node2D, scene : PackedScene):
-	var spawned_count_var : int
-	
-	# Summer
-	if node_marker == marker_summer:
-		spawned_count_var = spawned_count_summer
-	# Spring
-	elif node_marker == marker_spring:
-		spawned_count_var = spawned_count_spring
-	# Winter
-	elif node_marker == marker_winter:
-		spawned_count_var = spawned_count_winter
-	# Autumn
-	elif node_marker == marker_autumn:
-		spawned_count_var = spawned_count_autumn
-
-	for mark in node_marker.get_children():
-		if (spawned_count_var <= node_marker.get_child_count()):
+func spawn_resource(marker, scene):
+	var spawned_count = spawned_counts[marker.name]
+	for mark in marker.get_children() :
+		if spawned_count < marker.get_child_count() :
 			var tree_scene = scene.instantiate()
 			get_parent().call_deferred("add_child", tree_scene)
 			tree_scene.resource_node_queue_free.connect(_on_resource_queue_free)
 			tree_scene.global_position = mark.global_position
-			if node_marker == marker_summer:
-				spawned_count_summer += 1
-			elif node_marker == marker_spring:
-				spawned_count_spring += 1
-			elif node_marker == marker_winter:
-				spawned_count_winter += 1
-			elif node_marker == marker_autumn:
-				spawned_count_autumn += 1
+			spawned_counts[marker.name] += 1
+
+func _on_resource_queue_free(name) :
+	var node_name = name.to_lower().replace("_", "").replace(" ", "")
+	for season in tree_scenes.keys() :
+		if node_name == season.to_lower():
+			spawned_counts[season] -= 1
+			
+		if spawned_counts[season] == 0:
+			_spawn_tree_delayed(season)
 		
-func _on_resource_queue_free(name):
-	var node_name = name.to_lower().replace('_', '').replace(' ', '')
-	match node_name :
-		"coldtree" :
-			spawned_count_autumn -= 1
-		"orangetree":
-			spawned_count_spring -= 1
-		"pinktree":
-			spawned_count_winter -= 1
-		"normaltree":
-			spawned_count_spring -= 1
-	
-	if spawned_count_summer == 0 :
-		await get_tree().create_timer(5).timeout
-		spawn_resource(marker_summer, tree_summer)
-	
-	if spawned_count_autumn == 0 :
-		await get_tree().create_timer(5).timeout
-		spawn_resource(marker_autumn, tree_cold)
-	
-	if spawned_count_spring == 0 :
-		await get_tree().create_timer(5).timeout
-		spawn_resource(marker_spring, tree_orange)
-	
-	if spawned_count_winter == 0 :
-		await get_tree().create_timer(5).timeout
-		spawn_resource(marker_winter, tree_pink)
-		
+func _spawn_tree_delayed(season) :
+	var marker = get_node(season)
+	var tree_scene = tree_scenes[season]
+	await get_tree().create_timer(5).timeout
+	spawn_resource(marker, tree_scene)
 	
